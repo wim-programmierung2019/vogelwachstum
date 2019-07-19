a0e03240679bca061055db1c328fdde47d32152d
BendingUnit29 <jbharbers@t-online.de>
2019-07-19 15:30:50 +0000

    Binary stub

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     /**
      * Einstieg in ein Javaprogramm.
      */
+    static int popu = 0;
     public static void main(String[] args){
         // Im Indischen Ozean wurde eine insel entdeck, auf der sich eine 
         // bis dahin nicht bekannte Vogelart rasant vermehrt. Forscher
@@ -14,18 +15,23 @@ public class Main {
         // 3 | März    | 2
         // 4 | April   | 3
         // 5 | Mai     | 5
-        // 1 | Juni    | 8
+        // 6 | Juni    | 8
         // Die Forscher gehen davon aus, dass hinter dieser Beobachtung 
         // folgende Formel steckt:
         // p(m) = 0              wenn  m <= 0
         // p(m) = 1              wenn 0 < m <= 2
         // p(m) = p*(m-1)*(m-2)  sonst
+        int monat = 7;
         
-        int monat = 2;
         System.out.println(population(monat));
     }
     
     public static int population(int m){
-        return 0;
+        if(m==0){
+            return 0;
+        }else if(m==1){
+            return 1;
+        }else        
+        return population(m-1)+population(m-2);
     }
 }
